8b8e64a37ab8b4263ac336cf61069b15286d3cb2
Nick <nvanalfen2@gmail.com>
2022-07-07 13:41:37 -0400

    Made GUI functional

--- SudokuMainGUI.py
+++ SudokuMainGUI.py
@@ -2,8 +2,15 @@ import sys
 from functools import partial
 import numpy as np
 from PyQt5.Qt import QMainWindow, QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QGridLayout
 import PyQt5.QtWidgets
 from PyQt5 import QtCore, QtWidgets
+
+from Puzzles.Futoshiki import Futoshiki
+from Puzzles.Kropki import Kropki
+from Puzzles.Sudoku import Sudoku
+from Puzzles.KillerSudoku import KillerSudoku
+from Puzzles.KenKen import KenKen
 
 from Widgets.FutoshikiWidget import FutoshikiWidget
 from Widgets.KropkiWidget import KropkiWidget
@@ -12,26 +19,32 @@ from Widgets.RelationalSudokuWidget import PuzzleGridWidget
 class SudokuMainWindow(QMainWindow):
     def __init__(self):
         super(SudokuMainWindow, self).__init__()
-        #self.central_widget = QWidget()
-        self.puzzle_widget = KropkiWidget(parent=self)
-        self.mainLayout = QtWidgets.QVBoxLayout()
-        self.setLayout(self.mainLayout)
+        self.puzzle_widget = FutoshikiWidget(parent=self)
         self.central_widget = self.puzzle_widget
-        self.central_widget = PuzzleGridWidget(6, relations=["<",">"], vertical_relations=["^","v"], parent=self)
         self.setCentralWidget(self.central_widget)
-        #self.central_widget.setGeometry( 50, 50, self.central_widget.width(), self.central_widget.height() )
 
-        self.mainLayout.addWidget(self.central_widget)
         self.show()
-
-        #self.setFixedWidth(centralWidget.width() + 100)
-        #self.setFixedHeight(centralWidget.height() + 100)
     
-    def refresh_view(self):
-        self.temp = QPushButton(self,text="?")
-        self.temp.setGeometry( QtCore.QRect( 200, 10, 80, 30 ) )
-        print("?")
-        self.update()
+    def get_puzzle_type(self, puzzle_type):
+        if puzzle_type == "Futoshiki":
+            return Futoshiki( dimension=self.puzzle_widget.dim )
+        if puzzle_type == "Kropki":
+            return Kropki( dimension=self.puzzle_widget.dim )
+    
+    def solve(self, values):
+        puzzle_type = values["type"]
+        grid = values["grid"]
+        relations = None
+        if "relations" in values:
+            relations = values["relations"]
+
+        puzzle = self.get_puzzle_type(puzzle_type)
+        puzzle.load_grid(grid)
+        if not relations is None:
+            puzzle.load_relations(relations)
+        puzzle.solve()
+
+        self.puzzle_widget.puzzle_widget.set_grid( puzzle.grid )
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
